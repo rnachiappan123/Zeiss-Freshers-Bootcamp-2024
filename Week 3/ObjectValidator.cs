dee074847b959c1279dbca40e5fcbcda0fe453d0
Nachiappan Ramu <103435607+rnachiappan123@users.noreply.github.com>
2024-01-22 10:36:56 +0530

    Add files via upload

---
+++ Week 3/ObjectValidator.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Code_Assignment
+{
+    internal class ObjectValidator
+    {
+        public static bool Validate(Device deviceObj, out List<string> errors)
+        {
+            bool isValid = true;
+            var requiredAttributes = (Required[])typeof(Device).GetCustomAttributes(typeof(Required), true);
+            var rangeAttributes = (Range[])typeof(Device).GetCustomAttributes(typeof(Range), true);
+            var maxLengthAttributes = (MaxLength[])typeof(Device).GetCustomAttributes(typeof(MaxLength), true);
+            errors = new List<string>();
+
+            foreach ( var requiredAttribute in requiredAttributes)
+            {
+                if (deviceObj.Id == null)
+                {
+                    isValid = false;
+                    errors.Append(requiredAttribute.ErrorMessage);
+                }
+            }
+
+            foreach ( var rangeAttribute in rangeAttributes)
+            {
+                if (deviceObj.Code <= rangeAttribute.Start || deviceObj.Code >= rangeAttribute.End)
+                {
+                    isValid = false;
+                    errors.Append(rangeAttribute.ErrorMessage);
+                }
+            }
+
+            foreach ( var maxLengthAttribute in maxLengthAttributes)
+            {
+                if (deviceObj.Description.Length > maxLengthAttribute.Length)
+                {
+                    isValid = false;
+                    errors.Append(maxLengthAttribute.ErrorMessage);
+                }
+            }
+            return isValid;
+        }
+    }
+}
---
+++ Week 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code_Assignment
+{
+    class Device
+    {
+        [Required(ErrorMessage = "ID Property Requires Value")]
+        public string Id { get; set; }
+
+        [Range(10, 100, "Code Value Must Be Within 10-100")]
+        public int Code { get; set; }
+
+        [MaxLength(100, "Max of 100 Charcters are allowed")]
+        public string Description { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Device deviceObj = new Device();
+            deviceObj.Id = null;
+            deviceObj.Code = 0;
+            deviceObj.Description = "Hello";
+
+            List<string> errors;
+            bool isValid = ObjectValidator.Validate(deviceObj, out errors);
+            if (!isValid)
+            {
+                foreach (string item in errors)
+                {
+                    System.Console.WriteLine(item);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
